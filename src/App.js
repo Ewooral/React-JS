d430da9ec8e631549948af91834eccbfa8c939a3
Ewooral <boahen.cos@gmail.com>
2021-12-29 16:25:50 +0000

    just got prettier setup manually in the package.json

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
- 
- const Pet = (props) =>{
-     return React.createElement(
-         'div', {}, [
-         React.createElement( 'h2', {}, props.name),
-         React.createElement( 'h3', {}, props.age),
-         React.createElement( 'h3', {}, props.gender),
-         ]
-     )
- }
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.age),
+    React.createElement("h3", {}, props.gender),
+  ]);
+};
 
 const App = () => {
-      return  React.createElement(
-               'div',
-               {},
-               [
-                React.createElement("h1", {}, 'My first React app'),
-                // ...[1,2,3,4,5].map(x => React.createElement("h2", null, x)),
-                React.createElement(Pet, {name:" Luna", age:3, gender: "female" }),
-                React.createElement(Pet, {name:" Luna", age:3, gender: "female" }),
-                React.createElement(Pet, {name:" Luna", age:3, gender: "female" }),
-               ]
-       
-           )
-       }
-ReactDOM.render(
-    React.createElement(App), 
-    document.getElementById('root')
-)
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "My first React app"),
+    // ...[1,2,3,4,5].map(x => React.createElement("h2", null, x)),
+    React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
+    React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
+    React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
