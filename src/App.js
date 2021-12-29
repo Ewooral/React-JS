66de99e5260735d38d80d1f84ad85ff970c193c5
Ewooral <boahen.cos@gmail.com>
2021-12-29 03:44:39 +0000

    rendering of the Pet component three times

--- src/App.js
+++ src/App.js
@@ -13,11 +13,13 @@ const App = () => {
       return  React.createElement(
                'div',
                null,
-        [React.createElement( 'h1', {},'Hello World'
-        
-    
-        ),],
-               )
+               [
+                React.createElement("h1", {}, 'My first React app'),
+                React.createElement(Pet),
+                React.createElement(Pet),
+                React.createElement(Pet),
+               ]
+       
            )
        }
 ReactDOM.render(
