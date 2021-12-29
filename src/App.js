715f6024f56c1ed2a4f38a03a5a918e09d154393
Ewooral <boahen.cos@gmail.com>
2021-12-29 23:47:05 +0000

    just included packages and folders not needed in production into the .gitignore module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.age),
-    React.createElement("h3", {}, props.gender),
-  ]);
-};
+       import React from 'react';
+       import ReactDOM from 'react-dom';
+       
+       
+       const Pet = (props) => {
+        return React.createElement("div", {}, [
+            React.createElement("h2", {}, props.name),
+            React.createElement("h3", {}, props.age),
+            React.createElement("h3", {}, props.gender),
+        ]);
+        };
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "My first React app"),
-    // ...[1,2,3,4,5].map(x => React.createElement("h2", null, x)),
-    React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
-    React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
-    React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
-  ]);
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+        const App = () => {
+        return React.createElement("div", {}, [
+            React.createElement("h1", {}, "My first React app"),
+            // ...[1,2,3,4,5].map(x => React.createElement("h2", null, x)),
+            React.createElement(Pet, { name: " Luna", age: 4, gender: "female" }),
+            React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
+            React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
+        ]);
+        };
+        ReactDOM.render(React.createElement(App), document.getElementById("root"));
