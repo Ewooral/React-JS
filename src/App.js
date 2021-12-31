398c65d1872fa8b9c648aa81607ecd2d8185ec9e
Ewooral <boahen.cos@gmail.com>
2021-12-31 04:26:34 +0000

    set up hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-       import React from 'react';
+       import React from 'react';  // In the latest React version its not necessary to import React 
        import ReactDOM from 'react-dom';
        import Pet from "./Pet";
+       import SearchParams from './SearchParams.'
        
+        // const App = () => {
+        // return React.createElement("div", {}, [
+        //     React.createElement("h1", {}, "My first React app"),
+        //     // ...[1,2,3,4,5].map(x => React.createElement("h2", null, x)),
+        //     React.createElement(Pet, { name: " Luna", age: 4, gender: "female" }),
+        //     React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
+        //     React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
+        // ]);
+        // };
+        
         const App = () => {
-        return React.createElement("div", {}, [
-            React.createElement("h1", {}, "My first React app"),
-            // ...[1,2,3,4,5].map(x => React.createElement("h2", null, x)),
-            React.createElement(Pet, { name: " Luna", age: 4, gender: "female" }),
-            React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
-            React.createElement(Pet, { name: " Luna", age: 3, gender: "female" }),
-        ]);
-        };
-        ReactDOM.render(React.createElement(App), document.getElementById("root"));
+            return(
+                <div>
+                    <h1>My first React App </h1>
+                    <Pet name="Luna" age="4" gender="male"/>
+                    <Pet name="Aboagye" age="16" gender="male" />
+                    <Pet name="Akua Dompoh" age="54" gender="female" />
+
+                    <SearchParams />
+  
+                </div>
+            )
+        }
+        ReactDOM.render(<App/>, document.getElementById("root"));
