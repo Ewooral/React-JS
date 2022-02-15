8100af6ca81164fbceab78b7176a6a41511a7277
Ewooral <boahen.cos@gmail.com>
2022-02-15 22:08:35 +0000

    passing props to react component

--- src/passingPropsIntoComponents.js
+++ src/passingPropsIntoComponents.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
-
+// HEADER COMPONENT
 function Header(){
     return(
         <div>
             <h1>this is the Header page </h1>
         </div>
+    )
+}
+
+// MAIN COMPONENT
+function Main(){
+    return(
+        <div>This is the Main page</div>
+    )
+}
+
+// FOOTER COMPONENT
+function Footer(){
+    return(
+        <div>This is the Footer page</div>
     )
 }
 
