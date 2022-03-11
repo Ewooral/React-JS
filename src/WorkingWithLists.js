b1889daa185613e0e58d7bab7bca39ca95a9799f
Ewooral <boahen.cos@gmail.com>
2022-03-11 23:46:35 +0000

    working with Lists in React

---
+++ src/WorkingWithLists.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Lists = [
+    "Ga Beans",
+    "Milk and Tea",
+    "Rice with Stew",
+    "Banku with Tilapia"
+] 
+
+Lists.map((x) => {
+    console.log(x);
+})
+
+
+
+
+export default Lists
