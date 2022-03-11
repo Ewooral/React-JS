472edebfcb2e67642813f8a30f1ab6173551e00d
Ewooral <boahen.cos@gmail.com>
2022-03-11 00:57:04 +0000

    recapping on react basicsto advance

--- src/arrayDestructuring.js
+++ src/arrayDestructuring.js
@@ -3,10 +3,10 @@ import React from 'react'
 const [firstItem, secondItem, thirdItem] = ["boots", "tent", "headlamp"]  // destructuring 
 const [, , third] = ["Kamboo", "Socks", "hairbrush"]  // destructuring 
 
-let a = [1, 4, {1:"one", 2:"two"}, true, false, -1]
-a.map((x) => console.log(x) )
-console.log(a)
-console.log(b)
+// let [,,,,a, b] = [1, 4, {1:"one", 2:"two"}, true, false, -1]
+// a.map((x) => console.log(x) )
+// console.log(a)
+
 console.log(firstItem, third)
 
 
