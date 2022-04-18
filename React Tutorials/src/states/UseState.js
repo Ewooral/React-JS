fbc1ae343ccdea641050e798e4cf3f9349479428
Ewooral <boahen.cos@gmail.com>
2022-04-18 23:24:32 +0100

    React Hooks tutorials

--- React Tutorials/src/states/UseState.js
+++ React Tutorials/src/states/UseState.js
@@ -15,9 +15,19 @@ take a pair. the first one is a state variable
  */
    
    const UseState = () => {
+     const [bulb, setBulb] = useState("OFF")
+     console.log(bulb)
      return (
        <div>
           <h1>React Hooks Tutorials</h1>
+          {/* <h3>Currently, our light is {bulb}</h3> */}
+          <span>Click the buttons to turn the light on or off</span>
+
+          <div>
+            <button> OFF </button>
+            <button> OFF </button>
+
+          </div>
        </div>
      )
    }
