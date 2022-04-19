dbaa2f73fed69125f1f0f2499dd0c163430b1dd7
Ewooral <boahen.cos@gmail.com>
2022-04-19 01:13:34 +0100

    React Hooks tutorials

--- React Tutorials/src/states/UseState.js
+++ React Tutorials/src/states/UseState.js
@@ -1,23 +1,43 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useReducer} from 'react'
 
 /* 
 1. USESTATE 
 take a pair. the first one is a state variable 
  the second one is a function that updates the state
  We pass what we want the state to be in the useState("") function
- 2. USEEFFECT 
+
+2. USEEFFECT 
  it is used to manage side effects that are unrelated to the 
  component's render. E.g. console messages, loading data and animations
  can benefit from useEffect
    - useEffect takes in a callback function
    - it takes in a second arg called dependency array
 
+3. USEREDUCER
+The useReducer Hook is similar to the useState Hook.
+It allows for custom state logic.
+If you find yourself keeping track of multiple pieces of state that rely on complex logic, useReducer may be useful.
+
+Syntax =======
+The useReducer Hook accepts two arguments.
+useReducer(<reducer>, <initialState>)
+The reducer function contains your custom state logic and the initialStatecan be a simple value but generally will contain an object.
+The useReducer Hook returns the current stateand a dispatchmethod.
+Here is an example of useReducer in a counter app:
+
  */
 
  
    const UseState = () => {
+
      const [bulb, setBulb] = useState("OFF")
      const [emotions, setEmotions] = useState("SAD")
+    //  const [checked, setChecked] = useState(false)
+     const [checked, toggle] = useReducer(
+        (checked) => !checked, 
+          false
+    )
+
 
      useEffect(()=>{
         console.log(bulb)
@@ -26,25 +46,49 @@ take a pair. the first one is a state variable
      useEffect(()=>{
         console.log(emotions)
      }, [emotions] )
-     
 
-     return (
-       <div>
+    //  function toggle(){
+    //    setChecked((checked) => !checked)
+    //  }
+
+     
+    /** IF ternary operator
+     * 
+     * function example(…) {
+        return condition1 ? value1
+         : condition2 ? value2
+         : condition3 ? value3
+         : value4;
+}
+     */
+
+     return ( 
+       <div className='setbackground'>
           <h1>React Hooks Tutorials</h1>
-          <h3>Currently, our light is {bulb} and I am {emotions} at the same time </h3>
+          <h3>Currently, our light is <span id='_ui' className="white"> {bulb} </span> and I am {emotions} at the same time </h3>
 
           <span>Click the buttons to turn the light on or off</span>
 
           <div>
 
             <button onClick={() => {
-              setBulb("ON")
+              setBulb("ON")             
+              
+              _ui.className == "white" ? _ui.classList.replace("white", "yellow") : 
+              _ui.className == "off" ?   _ui.classList.replace("off", "yellow") : _ui.className == "white"
 
             }}> On </button>
 
 
             <button onClick={() => {
               setBulb("OFF")
+              
+              _ui.className == "white" ?  _ui.classList.replace("white", "off") : 
+              _ui.className == "yellow" ?   _ui.classList.replace("yellow", "off") : _ui.className == "white"
+
+
+
+
             }}> Off </button>
 
 
@@ -55,7 +99,20 @@ take a pair. the first one is a state variable
 
             <button onClick={() => {
               setEmotions("SAD")
-            }}> Sad </button>
+            }}> Sad </button> 
+
+              <br />
+              <br />
+
+            <label htmlFor="">Checkbox</label> 
+                      <input 
+                      type="checkbox" 
+                      value={checked} 
+                      name="checkbox" 
+                      id="checkbox" 
+                      onChange={toggle} 
+                      />
+            <h2>{ checked ? "checked!" : " not Checked!" }</h2>
 
           </div>
        </div>
