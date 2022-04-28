b0e227fd99cebe56feba0c861aaaf774a60164d5
Ewooral <boahen.cos@gmail.com>
2022-04-28 03:37:41 +0100

    react-router thorough tutorials

--- React Tutorials/src/states/UseState.js
+++ React Tutorials/src/states/UseState.js
@@ -1,19 +1,55 @@
 import React, {useState, useEffect, useReducer} from 'react'
 
+import { useSpring, animated } from 'react-spring'
+
+import styled from 'styled-components'
+
+ // Create a Title component that'll render an <h1> tag with some styles 
+            const Title = styled.h1`
+              font-size: 1.5em;
+              text-align: center;
+              color: palevioletred;
+            `;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+            const Wrapper = styled.section`
+              padding: 4em;
+              background: papayawhip;
+            `;
+
+            const Button = styled.button`
+              /* Adapt the colors based on primary prop */
+              background: ${props => props.primary ? "palevioletred" : "white"};
+              color: ${props => props.primary ? "white" : "palevioletred"};
+
+              font-size: 1em;
+              margin: 1em;
+              padding: 0.25em 1em;
+              border: 2px solid palevioletred;
+              border-radius: 3px;
+              cursor: pointer;
+              border-radius: 10px;
+            `;
+
+          
+
+// Use Title and Wrapper like any other React component – except they're styled! 
+
+
 /* 
-1. USESTATE 
+1. USE STATE 
 take a pair. the first one is a state variable 
  the second one is a function that updates the state
  We pass what we want the state to be in the useState("") function
 
-2. USEEFFECT 
+2. USE EFFECT 
  it is used to manage side effects that are unrelated to the 
  component's render. E.g. console messages, loading data and animations
  can benefit from useEffect
    - useEffect takes in a callback function
    - it takes in a second arg called dependency array
 
-3. USEREDUCER
+3. USE REDUCER
 The useReducer Hook is similar to the useState Hook.
 It allows for custom state logic.
 If you find yourself keeping track of multiple pieces of state that rely on complex logic, useReducer may be useful.
@@ -22,15 +58,42 @@ Syntax =======
 The useReducer Hook accepts two arguments.
 useReducer(<reducer>, <initialState>)
 The reducer function contains your custom state logic and the initialStatecan be a simple value but generally will contain an object.
-The useReducer Hook returns the current stateand a dispatchmethod.
+The useReducer Hook returns the current state and a dispatch method.
 Here is an example of useReducer in a counter app:
 
  */
+   
+
+// React Spring
+
+  const ReactSpring = () => {
+      const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 }, delay: {delay: 2} })
+  return <animated.div style={props}>I will fade in</animated.div>
+  }
+  
+
+  const Checked = () => {
+    return (
+      <>
+        <h2 style={{backgroundColor: "#afff00", padding:".5rem"}}> Subscribed! </h2>
+      </>
+    )
+  }
+
+  const NotChecked = () => {
+    return (
+      <>
+        <h2 style={{backgroundColor: "transparent"}}> Unsubscribed! </h2>
+      </>
+    )
+  }
 
  
    const UseState = () => {
 
-     const [bulb, setBulb] = useState("OFF")
+     
+
+     const [bulb, setBulb] = useState("undefined")
      const [emotions, setEmotions] = useState("SAD")
     //  const [checked, setChecked] = useState(false)
      const [checked, toggle] = useReducer(
@@ -60,10 +123,23 @@ Here is an example of useReducer in a counter app:
          : condition3 ? value3
          : value4;
 }
-     */
+      */
+ 
+
 
      return ( 
        <div className='setbackground'>
+
+                   <Wrapper>
+                      <Title>
+                        Hello World!
+                      </Title>
+                      <Button>
+                        Danger
+                      </Button>
+                     
+                  </Wrapper>
+          
           <h1>React Hooks Tutorials</h1>
           <h3>Currently, our light is <span id='_ui' className="white"> {bulb} </span> and I am {emotions} at the same time </h3>
 
@@ -104,7 +180,7 @@ Here is an example of useReducer in a counter app:
               <br />
               <br />
 
-            <label htmlFor="">Checkbox</label> 
+            <label htmlFor="">Subscribe</label> 
                       <input 
                       type="checkbox" 
                       value={checked} 
@@ -112,7 +188,11 @@ Here is an example of useReducer in a counter app:
                       id="checkbox" 
                       onChange={toggle} 
                       />
-            <h2>{ checked ? "checked!" : " not Checked!" }</h2>
+            <div className='wrap'> { checked ? <Checked /> : <NotChecked /> } </div>
+
+            <h4>React Spring</h4>
+            <h5><ReactSpring /></h5>
+             
 
           </div>
        </div>
